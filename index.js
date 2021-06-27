b1ba519be720df3e4ba198ff89d1f7bf02f75ef1
Joe Christensen <joechristensenwork@gmail.com>
2021-06-27 16:57:23 -0700

    commit tree 8cee552fd135cb19d2cc775985bab0af45bf42d6

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10);
+
+const num3 = 4;
+const num4 = 21;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 4, 20);
